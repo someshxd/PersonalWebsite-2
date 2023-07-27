ebee561a05dfe958085ec3a85551abe19d85bc14
Somesh <someshverma8055@gmail.com>
2023-07-27 15:43:48 +0530

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Topbar from "./Components/Topbar/Topbar";
 import MIdSection from "./Components/MidSection/MIdSection";
 import AboutMe from "./Components/AboutMe/AboutMe";
-import Portfolio from "./Components/Portfolio/portfolio";
+import Portfolio from "./Components/Portfolio/Portfolio";
 import Footer from "./Components/Footer/Footer";
 import Contact from "./Components/Contact/Contact.";
 
